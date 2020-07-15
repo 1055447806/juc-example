4f82dbd5a4ea65be547b5c72ea1d5ae497b6ac7a
Gary <1055447806@qq.com>
2020-07-16 00:40:36 +0800

    2020/07/16

---
+++ src/com/ohh/jcu/atomic/AtomicBooleanFlag.java
@@ -0,0 +1,21 @@
+package com.ohh.jcu.atomic;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class AtomicBooleanFlag {
+    /**
+     * 使用 AtomicBoolean 替代 加volatile关键字的boolean
+     */
+     private static final AtomicBoolean flag = new AtomicBoolean(true);
+
+     public static void main(String[] args) throws InterruptedException {
+         new Thread(()->{
+             while (flag.get()) {
+
+             }
+         }).start();
+         Thread.sleep(2000);
+         flag.set(false);
+         System.out.println("set the flag to false");
+     }
+}
