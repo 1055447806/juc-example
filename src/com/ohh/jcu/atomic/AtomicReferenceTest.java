9bd4007aa0e978333f53e1da61d403f96e8419da
Gary <1055447806@qq.com>
2020-07-16 21:50:18 +0800

    2020/07/16

---
+++ src/com/ohh/jcu/atomic/AtomicReferenceTest.java
@@ -0,0 +1,40 @@
+package com.ohh.jcu.atomic;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class AtomicReferenceTest {
+
+    public static void main(String[] args) {
+        AtomicReference<Simple> atomic = new AtomicReference<>(new Simple("Gary", 20));
+        System.out.println("atomic.get() = " + atomic.get());
+//        boolean success = atomic.compareAndSet(new Simple("Wu", 18), new Simple("ABC", 22));
+        boolean success = atomic.compareAndSet(atomic.get(), new Simple("ABC", 22));
+        System.out.println("success = " + success);
+    }
+
+    static class Simple {
+        private String name;
+        private int age;
+
+        public Simple(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        @Override
+        public String toString() {
+            return "Simple{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
+    }
+}
