8f3810e772e657b913558828fc53cbd56412f687
Gary <1055447806@qq.com>
2020-07-22 20:39:46 +0800

    2020/07/22

---
+++ src/com/ohh/jcu/atomic/AtomicIntegerArrayTest.java
@@ -0,0 +1,48 @@
+package com.ohh.jcu.atomic;
+
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicIntegerArray;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ *  AtomicLongArray 和 AtomicReferenceArray 与之相同
+ */
+public class AtomicIntegerArrayTest {
+
+    @Test
+    public void testCreate() {
+        AtomicIntegerArray array = new AtomicIntegerArray(10);
+        assertEquals(array.length(), 10);
+    }
+
+    @Test
+    public void testCreate2() {
+        int[] arr = new int[10];
+        AtomicIntegerArray array = new AtomicIntegerArray(arr);
+        assertEquals(array.length(), 10);
+    }
+
+    @Test
+    public void testGet() {
+        AtomicIntegerArray array = new AtomicIntegerArray(10);
+        assertEquals(0, array.get(5));
+    }
+
+    @Test
+    public void testSet() {
+        AtomicIntegerArray array = new AtomicIntegerArray(10);
+        assertEquals(0, array.get(5));
+        array.set(5, 5);
+        assertEquals(5, array.get(5));
+    }
+
+    @Test
+    public void testGetAndSet() {
+        AtomicIntegerArray array = new AtomicIntegerArray(10);
+        assertEquals(0, array.getAndSet(5, 5));
+        assertEquals(5, array.getAndSet(5, 5));
+    }
+
+}
---
+++ src/com/ohh/jcu/atomic/AtomicIntegerFieldUpdaterTest.java
@@ -0,0 +1,25 @@
+package com.ohh.jcu.atomic;
+
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+
+public class AtomicIntegerFieldUpdaterTest {
+
+    public static void main(String[] args) {
+        AtomicIntegerFieldUpdater<TestMe> updater = AtomicIntegerFieldUpdater.newUpdater(TestMe.class, "i");
+        TestMe me = new TestMe();
+        for (int i = 0; i < 2; i++) {
+            new Thread(() -> {
+                final int MAX = 20;
+                for (int j = 0; j < MAX; j++) {
+                    int v = updater.getAndIncrement(me);
+                    System.out.println(Thread.currentThread().getName() + "=>" + v);
+                }
+            }).start();
+        }
+    }
+
+    static class TestMe {
+        volatile int i;
+    }
+
+}
