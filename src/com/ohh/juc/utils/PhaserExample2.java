be2c4d82c33b2b9f7d229d9ff2b8a549e25592cf
Gary <1055447806@qq.com>
2020-08-19 16:26:52 +0800

    ~

---
+++ src/com/ohh/juc/utils/PhaserExample2.java
@@ -0,0 +1,59 @@
+package com.ohh.juc.utils;
+
+import java.util.Random;
+import java.util.concurrent.Phaser;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 非动态的
+ */
+public class PhaserExample2 {
+    private static final Random random = new Random(System.currentTimeMillis());
+
+    public static void main(String[] args) {
+        final Phaser phaser = new Phaser(5);
+        for (int i = 1; i < 6; i++) {
+            new Athletes(i, phaser).start();
+        }
+    }
+
+    static class Athletes extends Thread {
+        private final int no;
+        private final Phaser phaser;
+
+        Athletes(int no, Phaser phaser) {
+            this.no = no;
+            this.phaser = phaser;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(no + "start running...");
+            PhaserExample2.sleep(random.nextInt(5), TimeUnit.SECONDS);
+            System.out.println(no + "end running...");
+            System.out.println("this.phaser.getPhase() = " + this.phaser.getPhase());
+            this.phaser.arriveAndAwaitAdvance();
+
+            System.out.println(no + "start bicycle...");
+            PhaserExample2.sleep(random.nextInt(5), TimeUnit.SECONDS);
+            System.out.println(no + "end bicycle...");
+            System.out.println("this.phaser.getPhase() = " + this.phaser.getPhase());
+            this.phaser.arriveAndAwaitAdvance();
+
+            System.out.println(no + "start long jump...");
+            PhaserExample2.sleep(random.nextInt(5), TimeUnit.SECONDS);
+            System.out.println(no + "end long jump...");
+            System.out.println("this.phaser.getPhase() = " + this.phaser.getPhase());
+            this.phaser.arriveAndAwaitAdvance();
+            System.out.println("this.phaser.getPhase() = " + this.phaser.getPhase());
+        }
+    }
+
+    private static void sleep(long i, TimeUnit t) {
+        try {
+            t.sleep(i);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/com/ohh/juc/utils/PhaserExample4.java
@@ -0,0 +1,30 @@
+package com.ohh.juc.utils;
+
+import java.util.concurrent.Phaser;
+import java.util.concurrent.TimeUnit;
+
+public class PhaserExample4 {
+    public static void main(String[] args) throws InterruptedException {
+        final Phaser phaser = new Phaser(1);
+        /*for (int i = 0; i < 10; i++) {
+            System.out.println("phaser.getPhase() = " + phaser.getPhase());
+            phaser.arriveAndAwaitAdvance();
+        }*/
+        /*for (int i = 0; i < 10; i++) {
+            System.out.println("phaser.getRegisteredParties() = " + phaser.getRegisteredParties());
+            phaser.register();
+        }*/
+        /*System.out.println("phaser.getArrivedParties() = " + phaser.getArrivedParties());
+        System.out.println("phaser.getUnarrivedParties() = " + phaser.getUnarrivedParties());*/
+        /*phaser.bulkRegister(10);
+        System.out.println("phaser.getRegisteredParties() = " + phaser.getRegisteredParties());
+        System.out.println("phaser.getArrivedParties() = " + phaser.getArrivedParties());
+        System.out.println("phaser.getUnarrivedParties() = " + phaser.getUnarrivedParties());
+        new Thread(phaser::arriveAndAwaitAdvance).start();
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("phaser.getRegisteredParties() = " + phaser.getRegisteredParties());
+        System.out.println("phaser.getArrivedParties() = " + phaser.getArrivedParties());
+        System.out.println("phaser.getUnarrivedParties() = " + phaser.getUnarrivedParties());*/
+    }
+
+}
