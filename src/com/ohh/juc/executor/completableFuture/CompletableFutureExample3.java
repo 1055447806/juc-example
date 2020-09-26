5a29fdf6617a91cec80992161908608dbc430ab7
Gary <1055447806@qq.com>
2020-09-26 21:07:09 +0800

    ~

--- src/com/ohh/juc/executor/completableFuture/CompletableFutureExample3.java
+++ src/com/ohh/juc/executor/completableFuture/CompletableFutureExample3.java
@@ -1,14 +1,24 @@
 package com.ohh.juc.executor.completableFuture;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * CompletableFuture 的一些非组合方法。
+ * whenComplete，thenApply，handle，thenAccept，thenRun
+ */
 public class CompletableFutureExample3 {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
 //        testWhenComplete();
 //        testWhenCompleteAsync();
-        testThenApply();
+//        testThenApply();
+//        testThenApplyAsync();
+//        testHandleAsync();
+//        testThenAcceptAsync();
+        testThenRun();
+
         Thread.currentThread().join();
     }
 
@@ -48,19 +58,73 @@ public class CompletableFutureExample3 {
         System.out.println("task is submitted.");
     }
 
+    /**
+     * thenApply 方法会同步的处理一个值，并返回一个结果。
+     */
     private static void testThenApply() {
         CompletableFuture<Integer> future =
                 CompletableFuture
                         .supplyAsync(() -> "hello world.")
-                        .thenApply(String::length);
-        System.out.println("task is submitted.");
+                        .thenApply(v -> {
+                            try {
+                                TimeUnit.SECONDS.sleep(5);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            return v.length();
+                        })
+                        .whenComplete((v, t) -> System.out.println("v.length = " + v));
+        System.out.println("task is submitted");
+
     }
 
+    /**
+     * thenApplyAsync 方法会异步的处理一个值，并返回一个结果。
+     */
     private static void testThenApplyAsync() {
         CompletableFuture<Integer> future =
                 CompletableFuture
                         .supplyAsync(() -> "hello world.")
-                        .thenApplyAsync(String::length);
-        System.out.println("task is submitted.");
+                        .thenApplyAsync(v -> {
+                            try {
+                                TimeUnit.SECONDS.sleep(5);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            return v.length();
+                        })
+                        .whenComplete((v, t) -> System.out.println("v.length = " + v));
+        System.out.println("task is submitted");
+    }
+
+    /**
+     * handleAsync 和 handle 方法可以对程序中的错误进行处理。
+     */
+    private static void testHandleAsync() {
+        CompletableFuture
+                .<String>supplyAsync(() -> {
+                    throw new RuntimeException();
+                })
+                .handleAsync((v, t) -> (v == null) ? 0 : v.length())
+                .whenComplete((v, t) -> System.out.println("value is : " + v));
+        System.out.println("task is submitted");
+    }
+
+    /**
+     * thenAcceptAsync 和 thenAccept 方法消费一个 CompletableFuture 所产生的结果。
+     */
+    private static void testThenAcceptAsync() {
+        CompletableFuture
+                .supplyAsync(() -> "hello")
+                .thenAcceptAsync(System.out::println);
+    }
+
+    /**
+     * thenRunAsync 和 thenRun 方法可以运行一个 Runnable。
+     */
+    private static void testThenRun() {
+        CompletableFuture
+                .supplyAsync(() -> "hello")
+                .thenRun(() -> System.out.println("done."));
     }
 }
