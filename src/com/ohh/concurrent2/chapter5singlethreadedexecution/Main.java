0ec6e841c8e7ef1293ceddb775ba493e8db14bdb
1055447806 <1055447806@qq.com>
2021-01-17 04:15:48 +0800

    更新一下笔记

---
+++ src/com/ohh/concurrent2/chapter5singlethreadedexecution/Main.java
@@ -0,0 +1,32 @@
+package com.ohh.concurrent2.chapter5singlethreadedexecution;
+
+/**
+ * Single Threaded Execution 模式
+ * <p>
+ * 通过这个简单的例子，模拟多个线程对同一实例进行操作时的线程安全问题。
+ * <p>
+ * 其中 gate 是被多个线程共享的对象，每个线程在执行的时候都访问了 gate 的内部资源，
+ * 并对 gate 的内部资源进行了写操作，因此便引发了线程安全问题，在控制台中打印了大量的语句，
+ * 这些语句是不应该被打印的，所以应该对 Gate 类中的 pass 方法加锁，
+ * 使其对共享资源的读写操作串行化，以此来避免线程安全问题的产生。
+ * <p>
+ * 对共享的变量加锁
+ *
+ * @author Gary
+ */
+public class Main {
+    public static void main(String[] args) {
+        // 共享实例
+        Gate gate = new Gate();
+
+        // 定义三个线程
+        UserThread bj = new UserThread("Baobao", "BeiJing", gate);
+        UserThread sh = new UserThread("ShangLao", "ShangHai", gate);
+        UserThread gz = new UserThread("GuangLao", "GuangZhou", gate);
+
+        // 启动线程
+        bj.start();
+        sh.start();
+        gz.start();
+    }
+}
--- src/com/ohh/concurrent2/chapter5singlethreadedexecution/UserThread.java
+++ src/com/ohh/concurrent2/chapter5singlethreadedexecution/UserThread.java
@@ -1,19 +1,27 @@
-package com.ohh.concurrent2.chapter5;
+package com.ohh.concurrent2.chapter5singlethreadedexecution;
 
-public class User extends Thread {
+/**
+ * 模拟一个人，这个人不停的通过一个门
+ *
+ * @author Gary
+ */
+public class UserThread extends Thread {
+
     private final String myName;
     private final String myAddress;
     private final Gate gate;
 
-    public User(String myName, String myAddress, Gate gate) {
+    public UserThread(String myName, String myAddress, Gate gate) {
         this.myName = myName;
         this.myAddress = myAddress;
         this.gate = gate;
     }
 
+    /**
+     * 模拟一个人不停地通过一个门的操作
+     */
     @Override
     public void run() {
-        System.out.println(this.myName + " BEGIN");
         while (true) {
             this.gate.pass(this.myName, this.myAddress);
         }
