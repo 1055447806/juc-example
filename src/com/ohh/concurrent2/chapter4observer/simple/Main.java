9a40cb7218308547b045490d7a20743b20710349
1055447806 <1055447806@qq.com>
2021-01-17 03:14:26 +0800

    更新一下笔记

--- src/com/ohh/concurrent2/chapter4observer/simple/Main.java
+++ src/com/ohh/concurrent2/chapter4observer/simple/Main.java
@@ -3,7 +3,11 @@ package com.ohh.concurrent2.chapter4observer.simple;
 /**
  * Observer 模式
  * 实现了当对象状态发生变更时，对所有依赖于它的对象进行主动通知的意图。
- * <p>
+ * <pre>
+ *     +-----------+   If state is changed   +----------+
+ *     |  Subject  | +---------------------> | Observer |
+ *     +-----------+         notify()        +----------+
+ * </pre>
  * 定义一个主题事件，并对其添加两个监听者，BinaryObserver 和 OctalObserver
  *
  * @author Gary
