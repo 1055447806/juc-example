d0f7c0b04c61dd01a674316edfb11c7e9ddf8126
1055447806 <1055447806@qq.com>
2021-01-17 04:18:15 +0800

    更新一下笔记

--- src/com/ohh/concurrent2/chapter5singlethreadedexecution/Main.java
+++ src/com/ohh/concurrent2/chapter5singlethreadedexecution/Main.java
@@ -9,7 +9,6 @@ package com.ohh.concurrent2.chapter5singlethreadedexecution;
  * 并对 gate 的内部资源进行了写操作，因此便引发了线程安全问题，在控制台中打印了大量的语句，
  * 这些语句是不应该被打印的，所以应该对 Gate 类中的 pass 方法加锁，
  * 使其对共享对象内部的共享资源的读写操作串行化，以此来避免线程安全问题的产生。
- * <p>
  *
  * @author Gary
  */
